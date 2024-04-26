fe39718e339ffa393a5f38c689722c06527f46c4
Majidf0 <81880069+Majidf0@users.noreply.github.com>
2024-04-26 00:29:01 -0400

    Add files via upload

---
+++ Backend/Scripts/VideoRecognition.py
@@ -0,0 +1,40 @@
+import cv2  
+
+cap = cv2.VideoCapture('SteveJobsCut.mp4')
+face_cascade = cv2.CascadeClassifier('/opt/homebrew/Cellar/opencv/4.9.0_7/share/opencv4/haarcascades/haarcascade_frontalface_default.xml')
+#The above points to the harrcascade algorithm folder. It is tuned for my laptop, but the location might be different
+#in your machine. Since we arent using a container, just place the file path to the harrcascade, which is inside
+#your opencv installation 
+
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # CV has a video write method that outputs various codecs
+out = cv2.VideoWriter('CutVideo.mp4', fourcc, 20.0, (int(cap.get(3)), int(cap.get(4))))
+#
+
+# Read the video frame by frame
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Frame 2 Greyscale for algorithm to work properly
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Apply algorithm and detected faces
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    # Draw rectangles around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 5)
+
+    # Write frame to video file
+    out.write(frame)
+
+#Basically this code goes FRAME BY FRAME, treating each frame as an indiviual image. Whats really cool about this is
+#you do not need to learn anything new to video related processing, as it is literally just images over and over
+#again, analysis being repeated thousands of times for a single video.
+
+# Release
+cap.release()
+out.release()
+cv2.destroyAllWindows()
